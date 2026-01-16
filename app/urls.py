1fb30e82846e0caf318c13be3c6cc7ec52f96b3c
Marcelo <marceloftex@gmail.com>
2026-01-16 18:28:21 -0300

    Começando a camada de visualização

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+def home_view(request):
+    return render(
+        request,
+        'base.html'
+    )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", home_view),
 ]
