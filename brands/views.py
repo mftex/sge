b49da656192af730e31e8a405f9fcf288f33bd41
Marcelo <marceloftex@gmail.com>
2026-01-21 11:05:03 -0300

    Incluindo exclusão de brands e paginação

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . import forms
 from . import models
@@ -8,6 +8,7 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
     ordering = ['id']
 
     def get_queryset(self):
@@ -36,4 +37,10 @@ class BrandUpdateView(UpdateView):
     model = models.Brand
     template_name = 'brand_update.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
