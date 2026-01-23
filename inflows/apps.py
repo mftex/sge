2c03d7dcb9e612cc59591163db7654a0b72a4e32
Marcelo <marceloftex@gmail.com>
2026-01-23 13:22:06 -0300

    Incluindo signals para atualização do saldo em estoque a partir dos movimentos de entradas e saídas

--- inflows/apps.py
+++ inflows/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class InflowsConfig(AppConfig):
     name = "inflows"
+
+    def ready(self):
+        import inflows.signals 
+        
--- outflows/apps.py
+++ outflows/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class OutflowsConfig(AppConfig):
     name = "outflows"
+
+    def ready(self):
+        import outflows.signals 
+        
