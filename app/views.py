f9e6f7bdd47dbf32f356101d8d186349dcab06da
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-24 19:33:25 -0300

    Gráfico da home

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
+import json
 from django.shortcuts import render
 from . import metrics
 
 def home(request):
     product_metrics = metrics.get_product_metrics()
     sales_metrics = metrics.get_sales_metrics()
+    daily_sales_data = metrics.get_daily_sales_data()
     
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
+        'daily_sales_data': json.dumps(daily_sales_data),
     }
 
     return render(request, 'home.html', context)
