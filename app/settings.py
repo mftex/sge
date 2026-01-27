22e9014124ee2cc87d481383efdfd5b53e5f1c27
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-27 06:23:52 -0300

    Rota de Logout

--- app/settings.py
+++ app/settings.py
@@ -50,6 +50,8 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/login/'
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
     
     path("", views.home, name='home'),
     path("", include('brands.urls')),
